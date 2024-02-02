3eeb1ee4e07910932b31f1307a87a849b4320cd5
seojireung <jiyeong5599@daum.net>
2024-02-03 00:48:50 +0900

    회원 서비스

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 메서드 길어서 따로 뽑음
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            // ifPresent() : Optional<>의 메서드 (값이 존재하면~)
+                // findByName()의 결과가 Optional<Member>형임.
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+    * 회원 조회
+    */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
